d854b63935211cd9fd3d1ce852f8adb017543bc7
Camilovelag <camilo.vela.galindo@gmail.com>
2022-10-25 14:54:48 -0500

    Add style to calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -13,36 +13,36 @@ class Calculator extends Component {
     const { result } = this.state;
     return (
       <div className="calculator">
-        <p>{result}</p>
+        <p className="display">{result}</p>
         <div className="pad">
           <div>
-            <button type="button" className="button">AC</button>
-            <button type="button" className="button">+/-</button>
-            <button type="button" className="button">%</button>
-            <button type="button" className="button">+</button>
+            <button type="button" className="button gray">AC</button>
+            <button type="button" className="button gray">+/-</button>
+            <button type="button" className="button gray">%</button>
+            <button type="button" className="button orange">+</button>
           </div>
           <div>
-            <button type="button" className="button">7</button>
-            <button type="button" className="button">8</button>
-            <button type="button" className="button">9</button>
-            <button type="button" className="button">x</button>
+            <button type="button" className="button gray">8</button>
+            <button type="button" className="button gray">9</button>
+            <button type="button" className="button gray">7</button>
+            <button type="button" className="button orange">x</button>
           </div>
           <div>
-            <button type="button" className="button">4</button>
-            <button type="button" className="button">5</button>
-            <button type="button" className="button">6</button>
-            <button type="button" className="button">-</button>
+            <button type="button" className="button gray">4</button>
+            <button type="button" className="button gray">5</button>
+            <button type="button" className="button gray">6</button>
+            <button type="button" className="button orange">-</button>
           </div>
           <div>
-            <button type="button" className="button">1</button>
-            <button type="button" className="button">2</button>
-            <button type="button" className="button">3</button>
-            <button type="button" className="button">+</button>
+            <button type="button" className="button gray">1</button>
+            <button type="button" className="button gray">2</button>
+            <button type="button" className="button gray">3</button>
+            <button type="button" className="button orange">+</button>
           </div>
           <div>
-            <button type="button" className="button">0</button>
-            <button type="button" className="button">.</button>
-            <button type="button" className="button">=</button>
+            <button type="button" className="button wide gray">0</button>
+            <button type="button" className="button gray">.</button>
+            <button type="button" className="button orange">=</button>
           </div>
         </div>
       </div>
