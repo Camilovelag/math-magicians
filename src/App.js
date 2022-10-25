188c56f6f1b1d9045a250dda545d65a3760c8ded
Camilovelag <camilo.vela.galindo@gmail.com>
2022-10-25 17:51:02 -0500

    Use class insted pure function for App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+/* eslint-disable */
 import './App.css';
 import Calculator from './components/calculator';
+import { Component } from 'react';
+import { extend } from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
