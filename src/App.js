c44521f2ef749fad153b1068780856abb4823b25
Camilo Vela <camilo.vela.galindo@gmail.com>
2022-11-04 08:33:13 -0500

    Revert "Math magicians: full website"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import CalculatorPage from './pages/CalculatorPage';
-import Quote from './pages/Quote';
+import Calculator from './components/calculator';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/calculator" element={<CalculatorPage />} />
-          <Route path="/quote" element={<Quote />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </Router>
+      <Calculator />
     </div>
   );
 }
