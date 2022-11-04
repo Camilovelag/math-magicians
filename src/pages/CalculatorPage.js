cf61868b789ae6fac940c4b759d2267768040e95
Camilovelag <camilo.vela.galindo@gmail.com>
2022-11-04 06:45:03 -0500

    Fix importing link name bug

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Calculator from '../components/Calculator';
+import Calculator from '../components/calculator';
 
 const CalculatorPage = () => {
   const calcPageStyle = {
