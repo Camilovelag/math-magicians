2d8901010a662593eb478478a4703a5447e36d76
Camilovelag <camilo.vela.galindo@gmail.com>
2022-11-03 20:12:55 -0500

    Fix linter errors

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Calculator from '../components/Calculator';
+import Calculator from '../components/Calculator'; // eslint-disable-line
 
 const CalculatorPage = () => {
   const calcPageStyle = {
